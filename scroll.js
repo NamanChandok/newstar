57c4ed06c5b4daa9da2cd6ae999e0ec22f065898
Naman <namanchandok1@gmail.com>
2023-07-16 17:06:49 +0530

    master

--- scroll.js
+++ scroll.js
@@ -3,6 +3,11 @@ function updateProgress() {
     const {scrollTop, scrollHeight} = document.querySelector('main');
     const scrollPercent = scrollTop/(scrollHeight - window.innerHeight) * 100 + '%';
     document.querySelector('#progress-bar').style.setProperty('--progress', scrollPercent)
+    if (scrollTop > 700) {
+        document.querySelector('#navbar').style.setProperty('background-color', '#027373')
+    } else {
+        document.querySelector('#navbar').style.setProperty('background-color', 'transparent')
+    }
 }
 
 document.querySelector('main').addEventListener('scroll', updateProgress);
