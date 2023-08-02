2cc1e52634ed485a9080d7d7dba850529bf36860
Naman Chandok <namanchandok1@gmail.com>
2023-08-02 22:23:11 +0530

    dsadsa

--- tailwind-config.js
+++ tailwind-config.js
@@ -2,15 +2,13 @@ tailwind.config = {
     theme: {
         extend: {
             colors: {
-                'primary': "#2222ad",
-                'secondary': "#1b1b87",
-                'halftone': "#a4a4e0",
-                'light': "#fff",
                 'dark': "#0D0D0D",
-
             },
             fontFamily: {
-                'inter': ['Inter', 'sans-serif']
+                'inter': ['Inter', 'sans-serif'],
+                'cabin': ['Cabin', 'sans-serif'],
+                'madefor': ['Wix Madefor Display', 'sans-serif'],
+                'lora': ['Lora', 'serif'],
             }
         }
     }
